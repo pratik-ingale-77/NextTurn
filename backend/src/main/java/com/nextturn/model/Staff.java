63d7c8f64852284ed92c8e2fc14d0fd65cdc5b0c
Pratik ingale <ingalepratik2007@gmail.com>
2026-09-13 21:23:48 +0530

    Add Staff entity with fields and methods

---
+++ backend/src/main/java/com/nextturn/model/Staff.java
@@ -0,0 +1,55 @@
+package com.nextturn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String staffId;
+    private String password;
+
+    public Staff() {
+    }
+
+    public Staff(String fullName, String staffId, String password) {
+        this.fullName = fullName;
+        this.staffId = staffId;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
