3295019fd80f0668514265817ffe082fb67946d7
Pratik ingale <ingalepratik2007@gmail.com>
2026-09-13 20:26:30 +0530

    Initialize Spring Boot application

---
+++ backend/src/main/java/com/nextturn/NextTurnApplication.java
@@ -0,0 +1,12 @@
+package com.nextturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTurnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextTurnApplication.class, args);
+    }
+}
